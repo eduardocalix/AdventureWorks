928f935e8a9a2a6758b07ee8de32273b733b8315
CALIX <eduardocalix11xtra@gmail.com>
2018-10-13 15:25:18 -0600

    Agregar archivos de proyecto.

---
+++ AdventureWorkDepartamento/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdventureWorkDepartamento
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frmListarDepartamentos listar = new frmListarDepartamentos();
+            listar.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            frmCrearDepartamentos crear = new frmCrearDepartamentos();
+            crear.ShowDialog();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
